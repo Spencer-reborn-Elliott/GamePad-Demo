7950d864ee622cad581511d25ba31291c2811298
rebornman <fm_reborn@hotmail.com>
2020-04-10 00:23:15 +0100

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace GamePad_Demo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.OutputArea = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.PollForInputBtn = new System.Windows.Forms.Button();
+            this.OutputArea2 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // OutputArea
+            // 
+            this.OutputArea.Location = new System.Drawing.Point(21, 105);
+            this.OutputArea.Name = "OutputArea";
+            this.OutputArea.Size = new System.Drawing.Size(438, 454);
+            this.OutputArea.TabIndex = 0;
+            this.OutputArea.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(730, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "This is a sandbox for directInput using SharpDX. It should detect the first conne" +
+    "cted controller and detect input. It was designed with a dance mat in mind.";
+            // 
+            // PollForInputBtn
+            // 
+            this.PollForInputBtn.Location = new System.Drawing.Point(21, 52);
+            this.PollForInputBtn.Name = "PollForInputBtn";
+            this.PollForInputBtn.Size = new System.Drawing.Size(187, 47);
+            this.PollForInputBtn.TabIndex = 3;
+            this.PollForInputBtn.Text = "Detect Controller and Poll for Input";
+            this.PollForInputBtn.UseVisualStyleBackColor = true;
+            this.PollForInputBtn.Click += new System.EventHandler(this.PollForInputBtn_Click);
+            // 
+            // OutputArea2
+            // 
+            this.OutputArea2.Location = new System.Drawing.Point(484, 105);
+            this.OutputArea2.Name = "OutputArea2";
+            this.OutputArea2.Size = new System.Drawing.Size(616, 454);
+            this.OutputArea2.TabIndex = 4;
+            this.OutputArea2.Text = "";
+            this.OutputArea2.TextChanged += new System.EventHandler(this.OutputArea2_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1124, 582);
+            this.Controls.Add(this.OutputArea2);
+            this.Controls.Add(this.PollForInputBtn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.OutputArea);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Spencer\'s Dancemat/Joystick Detection";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox OutputArea;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button PollForInputBtn;
+        private System.Windows.Forms.RichTextBox OutputArea2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SharpDX.DirectInput;
+using System.Threading;
+
+
+/*  Turns out a dance mat can be detected! The buttons are weird as heck though. 
+ *  Here is a mapping
+ *  
+ *  Top left (x) is "Buttons6"
+ *  Up is "Buttons2"
+ *  Top right (o) is "Buttons7"
+ *  Left is  "Buttons0"
+ *  Middle is "Y"
+ *  Right is "Buttons3"
+ *  Down Left (Triangle) is "Buttons4"
+ *  Down is "Buttons1"
+ *  Down Right is "Buttons5"
+ *  Select is "Buttons8"
+ *  Start is "Buttons9"
+ *  Middle Area is "Y"
+ *  
+ *  
+ *  
+ *  For each button, the state "128" means it is pressed
+ *  The state "0" means it is not pressed
+ *  However, the middle button has it's own state (I fugure this is actually recognised as a stick tbh)
+ *  65535 is pressed and 32511 is not pressed
+ */
+
+
+
+namespace GamePad_Demo
+{
+    public partial class Form1 : Form
+    {
+        
+       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        void pollController()
+        {
+            Joystick TheMat;
+            string text = "";
+
+            var directInput = new DirectInput();
+            var joystickGuid = Guid.Empty;
+
+            //Find Gamepad
+            foreach (var deviceInstance in directInput.GetDevices(DeviceType.Gamepad, DeviceEnumerationFlags.AllDevices))
+            {
+                joystickGuid = deviceInstance.InstanceGuid;
+            }
+
+            // If Gamepad not found, look for a Joystick
+            if (joystickGuid == Guid.Empty)
+            {
+                foreach (var deviceInstance in directInput.GetDevices(DeviceType.Joystick, DeviceEnumerationFlags.AllDevices))
+                {
+                    joystickGuid = deviceInstance.InstanceGuid;
+                }
+            }
+            // If Joystick not found, throws an error
+            if (joystickGuid == Guid.Empty)
+            {
+                text = "No joystick/Gamepad found.\n";
+                OutputArea.Text = OutputArea.Text + text + "\n";
+                return;
+            }
+
+            // Instantiate the joystick
+
+            TheMat = new Joystick(directInput, joystickGuid);
+            text = "Found Joystick/Gamepad with GUID: " + joystickGuid + "\n";
+
+            // Query all suported ForceFeedback effects
+            var allEffects = TheMat.GetEffects();
+            if (allEffects.Count == 0)
+            {
+                text = text + "No effects available for this controller.\n";
+            }
+            foreach (var effectInfo in allEffects)
+            {
+                text = text + "Effect available: " + effectInfo.Name + "\n";
+            }
+
+
+            //Capabilities
+            text = text + "Axe Count: " + TheMat.Capabilities.AxeCount.ToString() + "\n";
+
+            text = text + "Button Count: " + TheMat.Capabilities.ButtonCount.ToString() + "\n";
+
+            text = text + "Driver Version: " + TheMat.Capabilities.DriverVersion.ToString() + "\n";
+
+            text = text + "Firmware Revision: " + TheMat.Capabilities.FirmwareRevision.ToString() + "\n";
+
+            text = text + "Force Feedback Minimum Time Resolution: " + TheMat.Capabilities.ForceFeedbackMinimumTimeResolution.ToString() + "\n";
+
+            text = text + "Force Feedback Sample Period: " + TheMat.Capabilities.ForceFeedbackSamplePeriod.ToString() + "\n";
+
+            text = text + "Hardware Revision: " + TheMat.Capabilities.HardwareRevision.ToString() + "\n";
+
+            text = text + "Pov Count: " + TheMat.Capabilities.PovCount.ToString() + "\n";
+
+            text = text + "Is Human Interface Device: " + TheMat.Capabilities.IsHumanInterfaceDevice.ToString() + "\n";
+
+            text = text + "Sub-Type: " + TheMat.Capabilities.Subtype.ToString() + "\n";
+
+
+            //Properies
+
+            text = text + "Axis Mode: " + TheMat.Properties.AxisMode.ToString() + "\n";
+
+            text = text + "Buffer Size: " + TheMat.Properties.BufferSize.ToString() + "\n";
+
+            text = text + "Class GUID: " + TheMat.Properties.ClassGuid.ToString() + "\n";
+
+            text = text + "Force Feedback Gain: " + TheMat.Properties.ForceFeedbackGain.ToString() + "\n";
+
+            text = text + "Interface Path: " + TheMat.Properties.InterfacePath.ToString() + "\n";
+
+            text = text + "Product name: " + TheMat.Properties.ProductName.ToString() + "\n";
+
+            text = text + "Joystick ID: " + TheMat.Properties.JoystickId.ToString() + "\n";
+
+            text = text + "Product ID: " + TheMat.Properties.ProductId.ToString() + "\n";
+
+            text = text + "Vendor ID: " + TheMat.Properties.VendorId.ToString() + "\n";
+
+
+            text = text + "Created Effects Count: " + TheMat.CreatedEffects.Count.ToString() + "\n";
+
+
+            text = text + "Usage: " + TheMat.Information.Usage.ToString() + "\n";
+
+            OutputArea.Text = OutputArea.Text + text + "\n";
+
+            // Set BufferSize in order to use buffered data.
+            TheMat.Properties.BufferSize = 128;
+            //TheMat.SetDataFormat(DeviceDataFormat.Joystick);
+
+            // Acquire the joystick
+            TheMat.Acquire();
+            //TheMat.SetCooperativeLevel(Handle, CooperativeLevel.Background | CooperativeLevel.NonExclusive);
+
+
+            while (1 == 1)
+            {
+                try
+                {
+                    TheMat.Poll();
+                    var stateData = TheMat.GetCurrentState();
+                    
+
+
+                    bool[] ButtonState = stateData.Buttons;
+
+                    for (int i = 0; i <= ButtonState.Length-1; i++)
+                    {
+                        if(ButtonState[i] == true)
+                        {
+                            OutputArea2.Text = OutputArea2.Text + "Button " + i + " pressed\n";
+                        }
+                    }
+
+
+                    //var bufferedData = TheMat.GetBufferedData();
+
+
+                    //This is totally useless, lol.
+                    //OutputArea2.Text = OutputArea2.Text + bData.ToString() + "\n";
+
+
+                    /*
+                     * This gets the raw buffered data. I do not want to mess around sorting through the string data returned, just the change in state will be fine thanks.
+                    foreach (var rawbuffereddata in bufferedData)
+                    {
+                        OutputArea2.Text = OutputArea2.Text + rawbuffereddata.ToString() + "\n";
+                    }
+                    */
+
+                }
+                catch
+                {
+
+                    OutputArea2.Text = OutputArea2.Text + "Disconnected. Attempting graceful controller exit.\n";
+                    TheMat.Dispose();
+                    OutputArea2.Text = OutputArea2.Text + "Disposed controller successfully, graceful disconnect achieved!\n";
+                    break;
+                }
+            }
+        }
+
+        public void JoystickInputThread()
+        {
+            try
+            {
+                pollController();
+            }
+            catch (Exception ex)
+            {
+                // log errors
+                OutputArea2.Text = OutputArea2.Text + ex.Message.ToString() + "\n";
+            }
+        }
+
+
+        private void PollForInputBtn_Click(object sender, EventArgs e)
+        {
+            //pollController();
+            Thread thread = new Thread(new ThreadStart(JoystickInputThread));
+            thread.Start();
+        }
+
+        private void OutputArea2_TextChanged(object sender, EventArgs e)
+        {
+            OutputArea2.SelectionStart = OutputArea2.Text.Length;
+            // scroll it automatically
+            OutputArea2.ScrollToCaret();
+        }
+    }
+}
